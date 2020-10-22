bdd0b49af17f71a36ffcb698afc8ba9860317e91
Shahab <Sz3_94@hotmail.com>
2020-10-22 14:15:06 +0100

    hooked up postgresql with Jack, made initial migrations, tableplus working

--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'turtles',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +76,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'shellbook-db',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
--- turtles/serializers/common.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from ..models import Turtle
-
-class TurtleSerializer(serializers.ModelSerializer): # converts data from models to valid JSON
-    
-    class Meta:
-        model = Turtle
-        fields = '__all__'
