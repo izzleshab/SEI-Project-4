7751e3d31b5cf9db114098ac06f63253766f4210
Shahab <Sz3_94@hotmail.com>
2020-10-22 13:36:09 +0100

    turtle model migrations made, and migrated

---
+++ turtles/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Turtle(models.Model):
+    name = models.CharField(max_length=50)
+    species = models.CharField(max_length=50)
+    diet = models.CharField(max_length=20)
+    image = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return f'{self.name} - {self.species}'
