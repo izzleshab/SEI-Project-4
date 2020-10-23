1ab2de6e08dea940d853cbde050527eb6b1e8747
Shahab <Sz3_94@hotmail.com>
2020-10-23 13:55:12 +0100

    comment serializer made, comment list view made, added to admin site, add comment to turtle working on admin site

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from .models import Comment
+
+from rest_framework.exceptions import NotFound, PermissionDenied
+from .serializers.common import CommentSerializer
 
 # Create your views here.
+
+class CommentListView(APIView):
+    
+    def post(self, request): # create comment
+        comment_to_create = CommentSerializer(data=request.data)
+        if comment_to_create.is_valid():
+            comment_to_create.save()
+            return Response(comment_to_create.data, status=status.HTTP_201_CREATED)
+        return Response(comment_to_create.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
