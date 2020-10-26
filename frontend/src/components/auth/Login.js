a4b1aa1a5a796333fdaf934eca7470a757480b9d
Shahab <Sz3_94@hotmail.com>
2020-10-26 15:47:23 +0000

    login working

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -1,11 +1,55 @@
 import React from 'react'
-// import { loginUser } from '../../lib/api'
-// import { setToken } from '../../lib/auth'
+import { loginUser } from '../../lib/api'
+import { setToken } from '../../lib/auth'
 
 class Login extends React.Component {
+  state = {
+    formData: {
+      email: '',
+      password: ''
+    }
+  }
+  
+  handleChange = event => {
+    const formData = {
+      ...this.state.formData,
+      [event.target.name]: event.target.value
+    }
+    this.setState({ formData })
+  }
+  
+  handleSubmit = async event => {
+    event.preventDefault()
+    const response = await loginUser(this.state.formData)
+    console.log(response)
+    setToken(response.data.token)
+    this.props.history.push('/turtles')
+  }
+  
   render() {
+    console.log(this.props)
+    const { email, password } = this.state.formData
     return (
-      'HELLO'
+      <section className="section">
+        <form onSubmit={this.handleSubmit}>
+          <input
+            placeholder="Email"
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+          /> 
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+          />
+          <div className="field">
+            <button type="submit" className="button is-fullwidth is-warning">Login</button>
+          </div>
+        </form>
+      </section>
     )
   }
 }
