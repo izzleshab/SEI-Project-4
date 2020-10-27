658f1cfe00a4404b1dab96e0474ebf19c1b3cebd
Shahab <Sz3_94@hotmail.com>
2020-10-27 11:17:56 +0000

    added name input for create a new turtle, working properly, will add the remaining inputs now

--- frontend/src/components/turtles/TurtleNew.js
+++ frontend/src/components/turtles/TurtleNew.js
@@ -1,17 +1,47 @@
 import React from 'react'
+import { createTurtle } from '../../lib/api'
+import TurtleForm from './TurtleForm'
 
-// import { createTurtle } from '../../lib/api'
-// import TurtleForm from './TurtleForm'
 
 class TurtleNew extends React.Component {
 
+  state = {
+    formData: {
+      name: '',
+      species: '',
+      diet: '',
+      image: ''
+    }
+  }
+  
+  handleChange = event => {
+    const formData = {
+      ...this.state.formData, 
+      [event.target.name]: event.target.value
+    }
+    this.setState({ formData })
+  }
 
+  handleSubmit = async event => {
+    event.preventDefault()
+    const response = await createTurtle(this.state.formData)
+    this.props.history.push(`/turtles/${response.data._id}`)
+  }
 
   render() {
     return (
-      'HELLO ITS WORKING'
+      <section className="section">
+        <div className="container">
+          <TurtleForm 
+            handleChange={this.handleChange}
+            handleSubmit={this.handleSubmit}
+            formData={this.state.formData}
+          />
+        </div>
+      </section>
     )
   }
+  
 }
 
 export default TurtleNew
